3f9628876f2526e33b1126dda8028a3e43b98a2b
rlara <rlara@incaztech.com>
2013-03-21 01:43:41 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fxmobiledj::Application.config.secret_token = '3dfa651eb23bee56e63cd85f01f4f209507418cd655066548953d01e72d1bb6fc89a73bd3a417915fbf510ea7c57fd12b7517318f86d35b0b8347fc9f4631623'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fxmobiledj::Application.config.session_store :cookie_store, key: '_fxmobiledj_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fxmobiledj::Application.config.session_store :active_record_store
